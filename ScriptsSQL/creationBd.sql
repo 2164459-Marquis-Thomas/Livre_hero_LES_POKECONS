125f0e2e3a8bfd2933bb831a4fa224e0b93babe9
Zo0mbi <2164459@carrefour.cegepvicto.ca>
2022-11-09 15:23:53 -0500

    Ajout de script

---
+++ ScriptsSQL/creationBd.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS Livre_hero;
+CREATE DATABASE Livre_hero;
+USE Livre_hero;
+
+CREATE TABLE livre(
+id int auto_increment primary key,
+nom varchar(255)
+);
+
+create table chapitre(
+id int auto_increment primary key,
+no_chapitre varchar(255) not null,
+texte text
+);
+
+create table lien_chapitre(
+id int auto_increment primary key,
+no_chapitre_origine varchar(255) not null,
+no_chapitre_destination varchar(255)
+);
+
+create table joueur_sauvegarde(
+id int auto_increment primary key,
+nom varchar(30) not null,
+chapitre_pogression varchar(255),
+point_de_vie int,
+combat int,
+endurance int
+);
